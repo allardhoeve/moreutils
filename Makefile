allardhoeve/moreutils

commit 0675c6ea5368f0843a91c28a472247b8f26015cf

Makefile: Warn users of pkgx.dev about their poor life choices
Their current installation script prevents installing man pages
(lol) by instead installing a .noop file (double lol). This allows
detecting it and warning the user.

I have requested that they not include moreutils.
https://github.com/pkgxdev/pantry/issues/5359

Evidence of their AI use.
https://github.com/pkgxdev/pantry/issues/5358

Makefile
@@ -24,6 +24,11 @@ isutf8: is_utf8/*.c is_utf8/*.h
 	cp is_utf8/isutf8 .
 
 install:
+	@if [ $(MANS) = .noop ]; then \
+		echo "pkgx.dev uses 'AI' to describe software, generate images, and respond to bug reports. That is gross. Please consider not using it. I will give you some time to think."; \
+		sleep 3600; \
+	fi
+
 	mkdir -p $(DESTDIR)$(PREFIX)/bin
 	$(INSTALL_BIN) $(BINS) $(DESTDIR)$(PREFIX)/bin
 	install $(PERLSCRIPTS) $(DESTDIR)$(PREFIX)/bin

commit b76308ce00440dfd88b82cc01b80814ffc12950b

Makefile: Fix shell syntax error on non-pkgx.dev builds
Add quotes to fix shell syntax for non-pkgx.dev builds, to prevent this
error message during 'make install':

    /bin/sh: 1: [: sponge.1: unexpected operator

Fixes: 0675c6ea5368 ("Makefile: Warn users of pkgx.dev about their poor life choices")
Signed-off-by: Nicolas Schier <nicolas@fjasle.eu>

Makefile
@@ -24,7 +24,7 @@ isutf8: is_utf8/*.c is_utf8/*.h
 	cp is_utf8/isutf8 .
 
 install:
-	@if [ $(MANS) = .noop ]; then \
+	@if [ "$(MANS)" = .noop ]; then \
 		echo "pkgx.dev uses 'AI' to describe software, generate images, and respond to bug reports. That is gross. Please consider not using it. I will give you some time to think."; \
 		sleep 3600; \
 	fi